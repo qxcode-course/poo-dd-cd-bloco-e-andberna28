cb2d28aa00ab8db0191148a154e052a7f2b7820b
Anderson <118529145+andberna28@users.noreply.github.com>
2025-12-01 19:21:53 +0000

    Correção na classe abstrata

--- tasks/poo/zoo/py/draft.py
+++ tasks/poo/zoo/py/draft.py
@@ -1,26 +1,28 @@
-# class Animal:
-#     def __init__(self, nome: str):
-#         self.nome = nome
+import abc
+
+class Animal(abc.ABC):
+    def __init__(self, nome: str):
+        self.nome = nome
         
-#     def aprensentar_nome(self):
-#         print(f"Eu me chamo {self.nome}")
+    def aprensentar_nome(self):
+        print(f"Eu me chamo {self.nome}")
 
-# class Tatu(Animal):
-#     def __init__(self, nome: str):
-#         super().__init__(nome)
+class Tatu(Animal):
+    def __init__(self, nome: str):
+        super().__init__(nome)
 
-#     def fazer_som(self):
-#         print("Faço *sons de tatu*")
+    def fazer_som(self):
+        print("Faço *sons de tatu*")
 
-#     def mover(self):
-#         print("Me encolho em formato de bola")
+    def mover(self):
+        print("Me encolho em formato de bola")
 
-#     def apresentar(self, animal: Animal):
-#         print(f"{self.aprensentar_nome}\n{self.fazer_som()}\n{self.mover()}\nTipo de animal: {animal}")
+    def apresentar(self, animal: Animal):
+        print(f"{self.aprensentar_nome}\n{self.fazer_som()}\n{self.mover()}\nTipo de animal: {animal}")
 
-#     def __str__(self):
-#         return f"{self.nome}"
+    def __str__(self):
+        return f"{self.nome}"
 
-# animal = Tatu("Rammus")
-# animal.aprensentar_nome()
-# animal.fazer_som()
+animal = Tatu("Rammus")
+animal.aprensentar_nome()
+animal.fazer_som()
